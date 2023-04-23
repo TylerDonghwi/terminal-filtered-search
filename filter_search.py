f5474337bfab4b895fe210f0c06a16b78b268129
Tyler Donghwi Kim <98932343+TylerDonghwi@users.noreply.github.com>
2023-04-23 16:55:38 +1200

    opens browser and prints the argument

--- filter_search.py
+++ filter_search.py
@@ -1,1 +1,13 @@
-print("python balls")
+import webbrowser
+import sys
+
+
+filter = []
+
+print(" ".join(sys.argv[1:]))
+
+url = 'http://pornhub.com/'
+
+chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+
+# webbrowser.get(chrome_path).open(url)
