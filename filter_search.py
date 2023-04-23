23857627df937c38e04206910b1c057c76575901
Tyler Donghwi Kim <98932343+TylerDonghwi@users.noreply.github.com>
2023-04-23 17:14:49 +1200

    cleaner

--- filter_search.py
+++ filter_search.py
@@ -2,10 +2,23 @@ import webbrowser
 import sys
 
 
-# filter = ['site:youtube.com']
+filter = ['reddit.com', 'stackoverflow.com', 'stackexchange.com', 'youtube.com', 'github.com', ]
 
-url = 'https://www.google.com/search?q=' + '+'.join(sys.argv[1:])
+def add_filter():
+  res = ' ('
+  for f in filter:
+    res += 'site:' + f
+    if (f != filter[-1]):
+      res += ' OR '
+    
+  return res + ')'
 
-chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
-
-webbrowser.get(chrome_path).open(url)
+def search():
+  if (len(sys.argv) == 1):
+    print("enter something to search for")
+  else:
+    url = 'https://www.google.com/search?q=' + '+'.join(sys.argv[1:]) + add_filter()
+    chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+    webbrowser.get(chrome_path).open(url)
+    
+search()
